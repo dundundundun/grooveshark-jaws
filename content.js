a02dc1b127b6a1b8e56aaa4b458fd4bce21ee602
ztbrown015 <zack.t.brown@gmail.com>
2012-10-05 16:28:38 -0400

    Added a method that will go out and get X tracks from each suggested artist.  This is still pretty rough.  It isn't clear to the user what number they are entering.  Needs to be tigher before issuing a pull request.

--- content.js
+++ content.js
@@ -5,9 +5,11 @@ GroovesharkProxy.setSongStatusCallback(function(status) {
 });
 
 $(document).ready(function(){
+	
+	//Get User's loved tracks
 	$(window).bind("lastFMGetLovedTracks", function(e, myname){
 	
-	$.get(
+		$.get(
 		"http://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks",
 		{'user': myname, 'api_key' : "e6e54bcc49f43a86ee275b5d831dde4e", 'format' : 'json', "limit" : "1000"},
 		function(response){
@@ -21,6 +23,40 @@ $(document).ready(function(){
 						var song = JSON.parse(response);
 						GroovesharkProxy.addSongsByID(song.SongID); 
 					}); 
+			});
+		});	
+
+	});
+
+	//Get User's recommended tracks from LastFM
+	$(window).bind("lastFMGetRecommendedTracks", function(e, params){
+		var temp_arr = params.split(',')
+		var myname = temp_arr[0]; 
+		var track_number = temp_arr[1];
+		console.log(myname); 
+		console.log(track_number); 
+		$.get(
+		"http://ws.audioscrobbler.com/2.0/?method=user.getnewreleases",
+		{'user': myname, 'api_key' : "e6e54bcc49f43a86ee275b5d831dde4e", 'format' : 'json', "limit" : "1000"},
+		function(response){
+			console.log(response); 
+			$.each(response.albums.album, function(index, album){
+				$.get(
+				"http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks",
+				{'artist': album.artist.name, 'api_key' : "e6e54bcc49f43a86ee275b5d831dde4e", 'format' : 'json', "limit" : track_number},
+				function(response){
+					console.log(response);
+					$.each(response.toptracks.track, function(index, track){
+						console.log("http://tinysong.com/b/" + track.artist.name + "%20" + track.name);
+						$.get(
+							"http://tinysong.com/b/" + track.artist.name + "%20" + track.name + "?",
+							{'key' : "2cd026068570503ea1e976782b551f9e", 'format' : 'json'},
+							function(response){
+								var song = JSON.parse(response);
+								GroovesharkProxy.addSongsByID(song.SongID); 
+							}); 
+					});
+				});
 			});
 		});	
 
